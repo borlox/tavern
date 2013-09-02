1412d54189b8dc5d3dcdda03a09e6484cd2f0c26
Jan <kuchen_@gmx.de>
2013-09-02 15:44:45 +0200

    Use some of the new Scripting functions in the test script

--- script/init.lua
+++ script/init.lua
@@ -1,2 +1,38 @@
 
 Log(LogLevel.Msg, "Hello world from Lua!")
+
+local EventHandler = {
+	handler = {},
+}
+_G.EventHandler = EventHandler
+
+function EventHandler:AddEventHandler(event, func)
+	if not self.handler[event] then
+		self.handler[event] = {}
+	end
+	
+	h = self.handler[event]
+	h[#h+1] = func
+end
+
+function EventHandler:HandleEvent(event, ...)
+	if self.handler[event] then
+		for _, func in ipairs(self.handler[event]) do
+			func(event, ...)
+		end
+	end
+end
+
+EventHandler:AddEventHandler("StateStart", function(event, state)
+	Log(LogLevel.Msg, "State start: " .. state)
+end)
+
+function Initialize()
+	local function HandleEvent(event, ...)
+		EventHandler:HandleEvent(event, ...)
+	end
+
+	SetEventHandler(HandleEvent)
+end
+
+Initialize()
