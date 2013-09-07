fb62eaa9aa53921827902716143295cb49df3d62
Jan Kuechler <kuchen_@gmx.de>
2013-09-07 18:27:09 +0200

    Use Tilemap::GetObjects to detect clicks on objects

--- script/init.lua
+++ script/init.lua
@@ -27,24 +27,42 @@ EventHandler:AddEventHandler("StateStart", function(event, state)
 	Log(LogLevel.Msg, "State start: " .. state)
 end)
 
+local function HandleObjectClick(object)
+	Log(LogLevel.Msg, "Object clicked: " .. object:GetName())
+end
+
+local function PointInRect(pt, topleft, dim)
+	return pt.x > topleft.x and pt.x <= (topleft.x + dim.x)
+	   and pt.y > topleft.y and pt.y <= (topleft.y + dim.y)
+end
+
 EventHandler:AddEventHandler("MouseButtonReleased", function(event, arg)
-	map = World:GetMap("")
-	tilePos = map:ScreenToTile(Vector2f(arg.x, arg.y))
+	local map = World:GetMap("")
+	local tilePos = map:ScreenToTile(Vector2f(arg.x, arg.y))
 
 	if not map:ContainsTilePos(tilePos) then
 		Log(LogLevel.Debug, "Click outside map")
 		return
 	end
 
-	if arg.button == MouseButton.Left then -- and map:IsTileAccessible(tilePos) then
-		hero = World:GetHero()
+	if arg.button == MouseButton.Left then
+		local objects = map:GetObjects("objects")
 
-		heroPos = Vector2f(hero:GetPosition())
-		path = map:FindPath(heroPos, tilePos)
+		for obj in objects do
+			local objpos = map:PixelToTile(Vector2f(obj:GetPosition()))
+			local objdim = map:PixelToTile(Vector2f(obj:GetDimensions().x, obj:GetDimensions().y))
+			Log(LogLevel.Msg, "Object: " .. obj:GetName() .. " | x = " .. objpos.x .. " | y = " .. objpos.y)
+			if PointInRect(tilePos, objpos, objdim) then
+				HandleObjectClick(obj)
+				return
+			end
+		end
+
+		local hero = World:GetHero()
+
+		local heroPos = Vector2f(hero:GetPosition())
+		local path = map:FindPath(heroPos, tilePos)
 		hero:FollowPath(path)
-		
-		--tilePos = map:ScreenToTile(Vector2f(arg.x, arg.y))
-		--hero:SetPosition(tilePos.x, tilePos.y)
 	end
 end)
 
